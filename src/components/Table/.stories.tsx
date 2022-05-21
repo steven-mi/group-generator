8f28a7267b7265627d3c7ca86b87e7df96456a70
steven-mi <stevenmi@live.de>
2022-05-21 22:41:08 +0200

    feature: add storybook

---
+++ src/components/Table/.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Table from '.';
+
+export default {
+  component: Table,
+  title: 'Table',
+};
+
+
+export const Default = () => <Table entries={["Test", "Test1", "Test2"]} />;
